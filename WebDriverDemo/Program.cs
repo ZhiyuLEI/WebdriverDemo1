10c67c8a536578ca888a8bb4efa5dd3ee47b5fc2
Zack <zack.lei.kiwi@gmail.com>
2017-11-16 07:56:33 +1300

    Installed IEDriverServer

--- WebDriverDemo/Program.cs
+++ WebDriverDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,7 @@ namespace WebDriverDemo
     {
         static void Main(string[] args)
         {
-            IWebDriver driver = new FirefoxDriver();
+            IWebDriver driver = new InternetExplorerDriver();
             driver.Url = "http://www.google.com";
         }
     }
